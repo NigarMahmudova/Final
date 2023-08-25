467e1cb07c9968dfeb0f335e8b41df8c74006fe0
NigarMahmudova <nigarem@code.edu.az>
2023-08-24 20:42:50 -0700

    created new mvc backend project and added crud for tables

---
+++ FamilyExperience/FamilyExperience/Entities/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FamilyExperience.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(35)]
+        public string Name { get; set; }
+
+        public List<Product> Products { get; set; }
+    }
+}
---
+++ FamilyExperience/FamilyExperience/Entities/ProductImage.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FamilyExperience.Entities
+{
+    public class ProductImage
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string ImageName { get; set; }
+        public bool? PosterStatus { get; set; }
+
+        public Product Product { get; set; }
+    }
+}
