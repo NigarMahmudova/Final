0977e9a1825cf101c1830bf6dc0c3a0922e9b34b
NigarMahmudova <nigarem@code.edu.az>
2023-08-31 15:47:29 -0700

    Feat: added quick view modal page

---
+++ FX-FrontEnd/assets/js/active.js
@@ -0,0 +1,36 @@
+// prodct details slider active
+$('.product-large-slider').slick({
+    fade: true,
+    arrows: false,
+    asNavFor: '.pro-nav'
+});
+
+
+// product details slider nav active
+$('.pro-nav').slick({
+    slidesToShow: 4,
+    asNavFor: '.product-large-slider',
+    arrows: false,
+    focusOnSelect: true
+});
+
+// image zoom effect
+$('.img-zoom').zoom();
+
+
+// pricing filter
+var rangeSlider = $(".price-range"),
+amount = $("#amount"),
+minPrice = rangeSlider.data('min'),
+maxPrice = rangeSlider.data('max');
+rangeSlider.slider({
+range: true,
+min: minPrice,
+max: maxPrice,
+values: [minPrice, maxPrice],
+slide: function (event, ui) {
+    amount.val("$" + ui.values[0] + " - $" + ui.values[1]);
+}
+});
+amount.val(" $" + rangeSlider.slider("values", 0) +
+" - $" + rangeSlider.slider("values", 1));
--- FX-FrontEnd/assets/js/app.js
+++ FX-FrontEnd/assets/js/app.js
@@ -1,3 +1,5 @@
+
+
 // Details page tabmenu
 
 let tabs = document.querySelectorAll('.tabs li');
@@ -23,24 +25,6 @@ for(let tab of tabs){
     }
 }
 
-// prodct details slider active
-$('.product-large-slider').slick({
-    fade: true,
-    arrows: false,
-    asNavFor: '.pro-nav'
-});
-
-
-// product details slider nav active
-$('.pro-nav').slick({
-    slidesToShow: 4,
-    asNavFor: '.product-large-slider',
-    arrows: false,
-    focusOnSelect: true
-});
-
-// image zoom effect
-$('.img-zoom').zoom();
 
 // Add to cart
 
