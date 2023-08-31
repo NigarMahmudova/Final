fd44a3cf411d9dd8d2323ce0f4c2b7f50b5006ff
NigarMahmudova <nigarem@code.edu.az>
2023-08-30 17:28:04 -0700

    Bug-fix: fixed bug in index and details slider

--- FX-FrontEnd/assets/js/app.js
+++ FX-FrontEnd/assets/js/app.js
@@ -23,6 +23,24 @@ for(let tab of tabs){
     }
 }
 
+// prodct details slider active
+$('.product-large-slider').slick({
+    fade: true,
+    arrows: false,
+    asNavFor: '.pro-nav'
+});
+
+
+// product details slider nav active
+$('.pro-nav').slick({
+    slidesToShow: 4,
+    asNavFor: '.product-large-slider',
+    arrows: false,
+    focusOnSelect: true
+});
+
+// image zoom effect
+$('.img-zoom').zoom();
 
 // Add to cart
 
